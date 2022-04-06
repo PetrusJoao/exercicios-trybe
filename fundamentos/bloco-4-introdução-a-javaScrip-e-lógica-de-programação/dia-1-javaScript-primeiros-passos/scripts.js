7426c56d64b7046da984d8f8b34b30456d5e08c4
João Pedro <joaopetrussousa@gmail.com>
2022-04-06 17:43:35 -0300

    Para fixar - JavaScript-Primeiros-passos

---
+++ fundamentos/bloco-4-introdução-a-javaScrip-e-lógica-de-programação/dia-1-javaScript-primeiros-passos/scripts.js
@@ -0,0 +1,99 @@
+// Variáveis
+
+/*let myName = "João";
+const birthCity = "São Luis";
+let birthYear = "1997";
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+birthYear = 2030;
+
+console.log(birthYear);
+
+// Erro ao tentar alterar uma constante.*/
+
+//Tipos Primitivos, Tipagem Dinâmica e Operações Aritméticas
+
+/*let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log(typeof patientId);
+console.log(typeof isEnrolled);
+console.log(typeof patientInfo);
+console.log(typeof patientEmail);
+
+let base = 5;
+let height = 8;
+
+let area = base * height;
+
+console.log(area);
+
+let perimeter = 2*base + 2*height;
+
+console.log(perimeter);*/
+
+// Condições If e Else
+
+/*let nota = 99;
+
+if (nota > 80){
+    console.log("Parabéns, você foi aprovada(o)!");
+}else if(nota < 80 && nota > 60){
+    console.log("Você está na nossa lista de espera.");
+}else{
+    console.log("Você foi reprovada(o)");
+}*/
+
+/*let currentHour = 16;
+let message;
+
+if (currentHour >= 22){
+    message = "Não deveríamos comer nada, é hora de dormir";
+}else if (currentHour >= 18 && currentHour < 22){
+    message = "Rango da noite, vamos jantar :D" ;
+}else if (currentHour >= 14 && currentHour < 18){
+    message = "Vamos fazer um bolo pro café da tarde?";
+}else if (currentHour >= 11 && currentHour < 14){
+    message = "Hora do almoço!!!";
+}else{
+    message = "Hmmm, cheiro de café recém passado";
+}
+
+console.log(message);
+
+let weekDay = "quarta-feira";
+
+if (weekDay == "segunda-feira" || weekDay == "terça-feira" || weekDay == "quarta-feira" || weekDay == "quinta-feira" || weekDay == "sexta-feira"){
+    console.log("Oba, mais um dia de aprendizado na Trybe >:D");
+}else {
+    console.log("FINALMENTE, descanso merecido UwU");
+}*/
+
+// Switch e Case
+
+/*let estadoSeletivo = "aprovado(a)";
+
+switch(estadoSeletivo){
+    case "aprovado(a)":
+        console.log("Parabéns, você foi aprovado(a)!");
+    break;
+
+    case "lista":
+        console.log("Você está na nossa lista de espera.");
+    break;
+
+    case "reprovado(a)":
+        console.log("Você foi reprovado(a)");
+    break;
+
+    default:
+        console.log("não se aplica");
+}*/
