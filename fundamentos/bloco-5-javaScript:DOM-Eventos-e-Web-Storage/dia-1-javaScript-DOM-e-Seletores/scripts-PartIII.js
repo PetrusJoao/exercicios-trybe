bbc211771af6d191fc71d60e743982d2e5f84d28
João Pedro <joaopetrussousa@gmail.com>
2022-04-13 18:49:26 -0300

    para fixar - DOM-Seletores de elementos-PartIII

---
+++ fundamentos/bloco-5-javaScript:DOM-Eventos-e-Web-Storage/dia-1-javaScript-DOM-e-Seletores/scripts-PartIII.js
@@ -0,0 +1,3 @@
+document.getElementById("header-container").style.display = "block"
+document.getElementById("header-container").style.marginLeft = "auto"
+document.getElementById("header-container").style.marginRight = "auto"
