a9e86ea07f435ad9808c1e6684d2e574c715c2e8
João Pedro <joaopetrussousa@gmail.com>
2022-05-13 15:17:36 -0300

    atualização exercicios/7.2

--- fundamentos/bloco-7-Introdução-à-JavaScript-ES6-e-Testes-Unitários/dia-2-JavaScript-ES6-Fluxo-de-exceção-e-Objetos/exercise-part-III.js
+++ fundamentos/bloco-7-Introdução-à-JavaScript-ES6-e-Testes-Unitários/dia-2-JavaScript-ES6-Fluxo-de-exceção-e-Objetos/exercise-part-III.js
@@ -30,19 +30,21 @@ const lesson1 = {
 // 2 - Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro.
 
     const listaKeys = (obj) => Object.entries(obj);
-    console.log(listaKeys(lesson1));
+    // console.log(listaKeys(lesson1));
 
 // 3 - Crie uma função para mostrar o tamanho de um objeto.
 
     const tamObj = (obj) => Object.keys(obj).length;
-    console.log(tamObj(lesson1));
+    // console.log(tamObj(lesson1));
 
 // 4 - Crie uma função para listar os valores de um objeto. Essa função deve receber um objeto como parâmetro.
 
     const valores = (obj) => Object.values(obj);
-    console.log(valores(lesson1));
+    // console.log(valores(lesson1));
 
 // 5 - Crie um objeto de nome allLessons, que deve agrupar todas as aulas através do Object.assign. Cada chave desse novo objeto será uma aula, sendo essas chaves: lesson1, lesson2 e lesson3. Ao executar o comando console.log(allLessons), a saída deverá ser a seguinte:
     
     const allLessons = Object.assign({}, {lesson1: lesson1}, {lesson2: lesson2}, {lesson3: lesson3});
     console.log(allLessons);
+
+// 6 - Usando o objeto criado no exercício 5, crie uma função que retorne o número total de estudantes em todas as aulas.
