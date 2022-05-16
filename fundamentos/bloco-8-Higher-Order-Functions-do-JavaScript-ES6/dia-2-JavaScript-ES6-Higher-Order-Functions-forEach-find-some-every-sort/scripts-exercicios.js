729c50a34fa33f661b3f6179bdd458d0a7cb1a98
João Pedro <joaopetrussousa@gmail.com>
2022-05-16 18:55:33 -0300

    exercicios/8.2-questão 6🚀

--- fundamentos/bloco-8-Higher-Order-Functions-do-JavaScript-ES6/dia-2-JavaScript-ES6-Higher-Order-Functions-forEach-find-some-every-sort/scripts-exercicios.js
+++ fundamentos/bloco-8-Higher-Order-Functions-do-JavaScript-ES6/dia-2-JavaScript-ES6-Higher-Order-Functions-forEach-find-some-every-sort/scripts-exercicios.js
@@ -73,10 +73,20 @@ const books = [
   
   // 5 - Faça uma função que retorne true, se todas as pessoas autoras nasceram no século XX, ou false, caso contrário.
 
-  const expectedResult = false;
+//   const expectedResult = false;
 
 function everyoneWasBornOnSecXX(sec) {
   return sec >= 1900;
 }
 
-console.log(books.every(everyoneWasBornOnSecXX));
+// console.log(books.every(everyoneWasBornOnSecXX));
+
+//6 - Faça uma função que retorne true, se algum livro foi lançado na década de 80, e false, caso contrário.
+
+// const expectedResult = true;
+
+function someBookWasReleaseOnThe80s(decade) {
+  return decade = 1980;
+}
+
+// console.log(books.some(someBookWasReleaseOnThe80s));
