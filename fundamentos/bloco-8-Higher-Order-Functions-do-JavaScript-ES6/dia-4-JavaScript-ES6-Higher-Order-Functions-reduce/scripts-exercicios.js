abf39912a4840fc0eee46f875e766c971f771851
João Pedro <joaopetrussousa@gmail.com>
2022-05-20 10:56:38 -0300

    exercicios/8.4-qerstão 2

--- fundamentos/bloco-8-Higher-Order-Functions-do-JavaScript-ES6/dia-4-JavaScript-ES6-Higher-Order-Functions-reduce/scripts-exercicios.js
+++ fundamentos/bloco-8-Higher-Order-Functions-do-JavaScript-ES6/dia-4-JavaScript-ES6-Higher-Order-Functions-reduce/scripts-exercicios.js
@@ -1,14 +1,92 @@
 // 1 - Dada uma matriz, transforme em um array.
 
 const arrays = [
-    ['1', '2', '3'],
-    [true],
-    [4, 5, 6],
-  ];
-  
-  function flatten(array) {
-    array.reduce((acc, curr) => acc + curr);
-    return array;
-  }
+  ['1', '2', '3'],
+  [true],
+  [4, 5, 6],
+];
+
+function flatten(array) {
+  array.reduce((acc, curr) => acc + curr);
+  return array;
+}
 
 // console.log(flatten(arrays));
+
+const books = [
+  {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: {
+      name: 'George R. R. Martin',
+      birthYear: 1948,
+    },
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: {
+      name: 'J. R. R. Tolkien',
+      birthYear: 1892,
+    },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Isaac Asimov',
+      birthYear: 1920,
+    },
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Frank Herbert',
+      birthYear: 1920,
+    },
+    releaseYear: 1965,
+  },
+  {
+    id: 5,
+    name: 'A Coisa',
+    genre: 'Terror',
+    author: {
+      name: 'Stephen King',
+      birthYear: 1947,
+    },
+    releaseYear: 1986,
+  },
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: {
+      name: 'H. P. Lovecraft',
+      birthYear: 1890,
+    },
+    releaseYear: 1928,
+  },
+];
+
+// 2 - Crie uma string com os nomes de todas as pessoas autoras.
+
+const expectedResult = "George R. R. Martin, J. R. R. Tolkien, Isaac Asimov, Frank Herbert, Stephen King, H. P. Lovecraft.";
+
+function reduceNames() {
+  const PessoasAutoras = books.reduce((acc, curr, index) => {
+    if (index == books.length-1){
+     return `${acc} ${curr.author.name}.`;
+    } return `${acc} ${curr.author.name},`
+  }, '');
+  return PessoasAutoras;
+}
+
+  console.log(reduceNames());
