012d78a7d3e854f5955397cb969c76c4b6c38474
João Pedro <joaopetrussousa@gmail.com>
2022-06-01 08:16:07 -0300

    pratica/9.1-exercicio-2

--- front-end/bloco-9-JavaScript-e-Testes-Assíncronos/dia-1-JavaScript-Assíncrono-e-Callbacks/scripts-pratica.js
+++ front-end/bloco-9-JavaScript-e-Testes-Assíncronos/dia-1-JavaScript-Assíncrono-e-Callbacks/scripts-pratica.js
@@ -1,10 +1,11 @@
-/* 1 - Adicione uma callback como parâmetro da função getUser.
+/* Para fixar 
+1 - Adicione uma callback como parâmetro da função getUser.
 No código abaixo você tem a função getUser, que define um objeto com os dados de uma pessoa. Complete a função getUser de forma que ela receba uma função callback como parâmetro para realizar as operações abaixo:
 Insira o retorno da função getUser;
 Complete a chamada da função getUser de modo que o retorno seja: "Hello! My name is Ivan Ivanovich";
 Complete a chamada da função getUser de modo que o retorno seja: "Ivan is Russian". */
 
-const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
+/* const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
 const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
 
 const getUser = (callback) => {
@@ -18,5 +19,68 @@ const getUser = (callback) => {
 };
 
 console.log(getUser(userFullName)); // complete a chamada da função getUser de modo que o retorno seja: "Hello! My name is Ivan Ivanovich"
-console.log(getUser(userNationality)); // complete a chamada da função getUser de modo que o retorno seja: "Ivan is Russian"
+console.log(getUser(userNationality)); // complete a chamada da função getUser de modo que o retorno seja: "Ivan is Russian" */
 
+/* 2 - Passe, como parâmetro e como retorno, uma callback para a função getUser.
+No código abaixo você tem a função getUser modificada, que agora funciona de modo assíncrono e imprime dados de uma pessoa depois de um certo tempo. Complete a função getUser de forma que ela receba uma callback como parâmetro e a retorne para que possa realizar as operações abaixo sobre a pessoa:
+Insira uma callback como parâmetro da função getUser;
+Retorne a callback passada como parâmetro na função getUser; */
+
+/* const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
+const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
+
+const delay = (maxMilliseconds = 5000) => Math.floor(Math.random() * maxMilliseconds);
+
+const getUser = (callback) => {
+  setTimeout(() => {
+    const user = {
+      firstName: "Ivan",
+      lastName: "Ivanovich",
+      nationality: "Russian",
+    };
+    // Retorne a `callback` passada como parâmetro na função `getUser`
+    // Dica: você pode manter o `console.log()`
+    console.log(callback(user));
+    return callback(user);
+  }, delay());  
+};
+
+getUser(userFullName); // deve imprimir "Hello! My name is Ivan Ivanovich" depois de um certo tempo
+getUser(userNationality); // deve imprimir "Ivan is Russian" depois de um certo tempo */
+ 
+/* Para fixar II
+Vamos botar tudo isso em prática com este exercício de fixação:
+1 - Adicione uma callback e trate o erro retornado.
+A função getCountry abaixo tem aproximadamente 50% de chance em obter, com sucesso, um país. Ela utiliza uma callback para poder realizar qualquer operação sobre o país retornado.
+Adicione um segundo parâmetro, que deve ser uma callback, na função getCountry;
+Retorne essa callback na função getCountry de forma que trate a mensagem de erro. */
+
+const countryName = ({ name }) => console.log(`Returned country is ${name}`);
+const countryCurrency = ({ name, currency }) => console.log(`${name}'s currency is the ${currency}`);
+
+const delay = (maxMilliseconds = 5000) => Math.floor(Math.random() * maxMilliseconds);
+
+const printErrorMessage = (error) => console.log(`Error getting country: ${error}`);
+
+const getCountry = (onSuccess, onFailure) => {
+  setTimeout(() => {
+    const didOperationSucceed = Math.random() >= 0.5;
+    if(didOperationSucceed) {
+      const country = {
+        name: "Brazil",
+        hdi: 0.759,
+        currency: "Real",
+      };
+      onSuccess(country);
+    } else {      
+      const errorMessage = "Country could not be found";
+      onFailure(errorMessage);
+    }
+  }, delay());
+};
+
+// Deve imprimir "Returned country is Brazil" no sucesso ou "Error getting country: Country could not be found" em caso de falha
+getCountry(countryName, printErrorMessage);
+
+// Deve imprimir "Brazil's currency is the Real" no sucesso, ou "Error getting country: Country could not be found" em caso de falha
+getCountry(countryCurrency, printErrorMessage);
